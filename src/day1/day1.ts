322851fb7f5e3c9333c414248700512b263f4506
robcronin <robc@theodo.co.uk>
2020-12-06 14:35:10 +0000

    Clean up returns on previous day answers

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,29 +1,29 @@
 export const day1 = (input: number[]) => {
   let result = 0;
-  input.some((m) => {
-    return input.some((n) => {
+  input.some((m) =>
+    input.some((n) => {
       if (m + n === 2020) {
         result = m * n;
         return true;
       }
-    });
-  });
+    }),
+  );
 
   return result;
 };
 
 export const day1part2 = (input: number[]) => {
   let result = 0;
-  input.some((m) => {
-    return input.some((n) => {
-      return input.some((p) => {
+  input.some((m) =>
+    input.some((n) =>
+      input.some((p) => {
         if (m + n + p === 2020) {
           result = m * n * p;
           return true;
         }
-      });
-    });
-  });
+      }),
+    ),
+  );
 
   return result;
 };
--- src/day3/day3.ts
+++ src/day3/day3.ts
@@ -1,14 +1,10 @@
 import { countTreesOnSlope } from '../utils/map';
 
-export const day3 = (input: string[]) => {
-  return countTreesOnSlope(input, 3, 1);
-};
-export const day3part2 = (input: string[]) => {
-  return (
-    countTreesOnSlope(input, 1, 1) *
-    countTreesOnSlope(input, 3, 1) *
-    countTreesOnSlope(input, 5, 1) *
-    countTreesOnSlope(input, 7, 1) *
-    countTreesOnSlope(input, 1, 2)
-  );
-};
+export const day3 = (input: string[]) => countTreesOnSlope(input, 3, 1);
+
+export const day3part2 = (input: string[]) =>
+  countTreesOnSlope(input, 1, 1) *
+  countTreesOnSlope(input, 3, 1) *
+  countTreesOnSlope(input, 5, 1) *
+  countTreesOnSlope(input, 7, 1) *
+  countTreesOnSlope(input, 1, 2);
